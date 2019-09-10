7331f5190da0f847c1ee21173fdb5c11d183ea6d
a0988986570 <a0988986570@gmail.com>
2019-09-10 15:13:01 +0800

    上傳python 資料

---
+++ datatype.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 11:09:00 2019
+
+@author: user
+"""
+#資料:程式的基本單位
+#數字
+3456
+3.5
+#字串
+"測試中文"
+"HELLO"
+#布林值
+True
+False
+#有順序、可動的列表list
+[3,4,5]
+["hello","world"]
+#有順序、不可動的列表Tuple
+(3,4,5)
+("hello","world")
+#集合Set、沒有順序概念
+{3,4,5}
+{"hello","world"}
+#字典 Dictionary
+{"apple":"蘋果","data":"資料"}
+
+#變數:用來存資料的自訂名稱
+#變數名稱=資料
+x=3
+#print(資料)
+print(["hello","world"])
+print(x)
+x=True #取代舊資料
+print(x)
+x={3,5,6}
+print(x)
---
+++ start.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 10:46:06 2019
+
+@author: user
+"""
+#註解符號
+print("Hello world")
