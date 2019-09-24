9732f7f83798c18a10b7b308f8a5d8316f948cc6
a0988986570 <a0988986570@gmail.com>
2019-09-24 19:58:07 +0800

    lotto_craawler

--- python_網路連線與網頁資料擷取與分析/python_爬蟲/lotto_crawler.py
+++ python_網路連線與網頁資料擷取與分析/python_爬蟲/lotto_crawler.py
@@ -19,4 +19,17 @@ htmldata=html.text
 data=bs(htmldata,"html.parser")
 
 data1=data.select("#rightdown") #找到id="rightdown"的標籤儲存成List，傳回data1
-print(data1)
+#print(data1)
+data2=data1[0].find("div",{"class":"contents_box02"})
+#print(data2)
+data3=data2.find_all("div",{"class":"ball_tx ball_green"})
+#print(data3)
+print("開出順序:",end=" ") #end結束字元，預設為"\n"
+for n in range(0,6):
+    print(data3[n].text,end=" ")
+print("")
+print("大小順序:",end=" ")
+for n in range(6,12):
+    print(data3[n].text,end=" ")
+print("")
+print("第2區:",data2.find("div",{"class":"ball_red"}).text)
